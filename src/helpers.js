5cebaef5221bfad594321a37281f82d63bccac2b
mohamadD <rediab88@gmail.com>
2019-11-14 19:02:52 +0200

    last

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-import { token } from "./token";
+//import { token } from "./token";
 // const token = "b66a27caa6ad9aaa0e80eda197d282bb0005ad71";
 const checkResponse = response => {
   if (response.status !== 200 && response.status !== 404) {
@@ -12,9 +12,7 @@ const checkResponse = response => {
 };
 
 export default function fetchByName(user_name) {
-  return fetch(
-    `https://api.github.com/users/${user_name}?access_token=${token}`
-  )
+  return fetch(`https://api.github.com/users/${user_name}`)
     .then(checkResponse)
 
     .catch(err => {
